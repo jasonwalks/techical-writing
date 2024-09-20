50d2e060ef8c787d4ef03da3e1745cf8397eb439
jasonwalks <43026914+jasonwalks@users.noreply.github.com>
2024-09-20 16:54:15 +0800

    Create readDocx.py

---
+++ readDocx.py
@@ -0,0 +1,13 @@
+# Get the Full text for a .docx file. 
+
+#! python3
+
+import docx
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)”
+
